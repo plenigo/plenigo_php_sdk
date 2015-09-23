aee5d34366965ed9fb44d2eb0afc25227bb424f4
Sebastian Dieguez <s.dieguez@plenigo.com>
2015-09-23 13:36:26 -0300

    - Comments and formatting

--- src/plenigo/models/AppAccessData.php
+++ src/plenigo/models/AppAccessData.php
@@ -56,6 +56,7 @@ class AppAccessData {
      * Maps the respons of the GetAllApps call to a list of single access objects
      * 
      * @param array $map the array of maps representing the objects
+     * 
      * @return array an array of AppAccessData objects
      */
     public static function createFromMapArray(array $map = array()) {
@@ -71,6 +72,7 @@ class AppAccessData {
      * Maps an associative array to the AppAccessData object
      * 
      * @param array $map an associative array representing the AppAccessData object
+     * 
      * @return \plenigo\models\AppAccessData
      */
     public static function createFromMap($map) {
@@ -82,34 +84,74 @@ class AppAccessData {
         return new AppAccessData($customerId, $customerAppId, $productId, $description);
     }
 
+    /**
+     * Gets the Customer ID
+     * 
+     * @return string The Customer ID
+     */
     public function getCustomerId() {
         return $this->customerId;
     }
 
+    /**
+     * Gets the customer App ID
+     * 
+     * @return string The customer App ID
+     */
     public function getCustomerAppId() {
         return $this->customerAppId;
     }
 
+    /**
+     * Gets the Product ID for this App
+     * 
+     * @return string The Product ID for this App
+     */
     public function getProductId() {
         return $this->productId;
     }
 
+    /**
+     * Gets the App ID description
+     * 
+     * @return string The App ID description
+     */
     public function getDescription() {
         return $this->description;
     }
 
+    /**
+     * Sets the Customer ID
+     * 
+     * @param string $customerId The New Customer ID
+     */
     public function setCustomerId($customerId) {
         $this->customerId = $customerId;
     }
 
+    /**
+     * Sets the customer App ID
+     * 
+     * @param string $customerAppId The New customer App ID
+     */
     public function setCustomerAppId($customerAppId) {
         $this->customerAppId = $customerAppId;
     }
 
+    /**
+     * Sets the product ID for this App
+     * 
+     * @param string $productId The New product ID for this App
+     */
     public function setProductId($productId) {
         $this->productId = $productId;
     }
 
+    /**
+     * Sets the App ID description
+     * 
+     * @param string $description The New App ID description
+     */
     public function setDescription($description) {
         $this->description = $description;
     }
