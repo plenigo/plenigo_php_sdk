a5bc33ec78702b47cf82f9cb56af1f8faab59bdb
Sebastian Dieguez <s.dieguez@plenigo.com>
2016-11-22 11:31:17 -0300

    New parameters for the checkout functions

--- src/plenigo/builders/CheckoutSnippetBuilder.php
+++ src/plenigo/builders/CheckoutSnippetBuilder.php
@@ -49,14 +49,17 @@ class CheckoutSnippetBuilder {
      * options have been selected, this will produce a Javascript snippet of
      * code that can be used as an event on a webpage.
      *
-     * @param array $settings A map of settings to pass to the Checkout service interface.
-     * @param string $loginToken A string contianing the loginToken if obtained from the external user management
-     * @param bool $showRegisterFirst Shows a register screen if the user is not loged in
-     *
+     * @param array $settings A map of settings to pass to the Checkout service interface
+     * @param string $loginToken A string contianing the loginToken (if obtained from the external user management) or null
+     * @param bool $showRegisterFirst Shows a register screen if the user is not loged in (default: false)
+     * @param string $sourceUrl URL of the page that lead to that checkout
+     * @param string $targetUrl URL of the page the process should redirect to after successful payment
+     * @param string $affiliateId affiliate id to associate with this checkout
+     * 
      * @return string A Javascript snippet that is compliant with plenigo's Javascript SDK.
      * @throws CryptographyException When an error occurs during data encoding
      */
-    public function build($settings = array(), $loginToken = null, $showRegisterFirst = false) {
+    public function build($settings = array(), $loginToken = null, $showRegisterFirst = false, $sourceUrl = null, $targetUrl = null, $affiliateId = null) {
         $clazz = get_class();
         PlenigoManager::get()->notice($clazz, "Building CHECKOUT snippet:");
         //Add testMode SDK check
@@ -67,16 +70,47 @@ class CheckoutSnippetBuilder {
         $requestQueryString = $this->buildCheckoutRequestQueryString($settings);
         $encodedData = $this->buildEncodedData($requestQueryString);
         PlenigoManager::get()->notice($clazz, "Checkout QUERYSTRING:" . $requestQueryString);
-        if (is_null($loginToken)) {
-            if($showRegisterFirst){
-                return sprintf("plenigo.checkout('%s', true);", $encodedData);
-            }else{
-                return sprintf("plenigo.checkout('%s');", $encodedData);
-            }
-        } else {
-            PlenigoManager::get()->notice($clazz, "Login TOKEN:" . $loginToken);
-            return sprintf("plenigo.checkoutWithRemoteLogin('%s','%s');", $encodedData,$loginToken);
+
+        $strFunction = "plenigo.checkout";
+        $strFirstParam = "";
+        if ($showRegisterFirst) {
+            $strFirstParam = ", true";
         }
+        if (!is_null($loginToken)) {
+            PlenigoManager::get()->notice($clazz, "Login TOKEN:" . $loginToken);
+            $strFunction = "plenigo.checkoutWithRemoteLogin";
+            $strFirstParam = ", '" . $loginToken . "'";
+        }
+        $strSurceURL = "";
+        $strTargetURL = "";
+        if (!is_null($sourceUrl)) {
+            PlenigoManager::get()->notice($clazz, "Source URL:" . $sourceUrl);
+            $strSurceURL = ", '" . $sourceUrl . "'";
+            if (!$showRegisterFirst && is_null($loginToken)) {
+                $strFirstParam = ", false";
+            }
+        }
+        if (!is_null($targetUrl)) {
+            PlenigoManager::get()->notice($clazz, "Target URL:" . $targetUrl);
+            $strTargetURL = ", '" . $targetUrl . "'";
+            if (is_null($sourceUrl)) {
+                $strSurceURL = ", null";
+            }
+        }
+        $strAffiliate = "";
+        if (!is_null($affiliateId)) {
+            PlenigoManager::get()->notice($clazz, "Affiliate ID:" . $affiliateId);
+            $strAffiliate = ", '" . $affiliateId . "'";
+            if (is_null($sourceUrl)) {
+                $strSurceURL = ", null";
+            }
+            if (is_null($targetUrl)) {
+                $strTargetURL = ", null";
+            }
+        }
+
+        $strFunctionFormula = $strFunction . "('%s'" . $strFirstParam . $strSurceURL . $strTargetURL . $strAffiliate . ");";
+        return sprintf($strFunctionFormula, $encodedData);
     }
 
     /**
