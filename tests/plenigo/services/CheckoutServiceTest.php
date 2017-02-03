53e8350abc8a209b05c10c67394e42102a5df5b3
Sebastian Dieguez <sebasoft@gmail.com>
2017-02-03 07:36:51 -0300

    All test ready to go

--- tests/plenigo/services/CheckoutServiceTest.php
+++ tests/plenigo/services/CheckoutServiceTest.php
@@ -25,6 +25,7 @@ use \plenigo\internal\ApiResults;
 class CheckoutServiceTest extends PlenigoTestCase {
 
     const PROD_ID_1 = "fuwUyCq8281643736141";
+    const VOUCHER_ID_1 = "asd123";
     const CUSTOMER_ID = 'MAP27KCA3K2P';
     const SECRET_ID = 'AMXzF7qJ9y0uuz2IawRIk6ZMLVeYKq9yXh7lURXQ';
     const COMPANY_ID = 'h7evZBaXvhaLVHYRTIHD';
@@ -52,11 +53,36 @@ class CheckoutServiceTest extends PlenigoTestCase {
     }
 
     public function testBuyFreeProductFalse() {
-        $data = json_decode('{"error":"403","description":"Access is not allowed"}');
+        $data = json_decode('{"error":"404","description":"Product ID not found"}');
         CheckoutServiceMock::$requestResponse = $data;
 
         try {
             $result = CheckoutServiceMock::buyFreeProduct(self::PROD_ID_1, self::CUSTOMER_ID, false);
+            $this->assertTrue(false, "Method should have thrown an error");
+        } catch (\Exception $exc) {
+            $this->assertTrue(true);
+        }
+
+        $this->assertError(E_USER_NOTICE, "GET URL CALL");
+    }
+
+    public function testRedeemVoucherTrue() {
+        CheckoutServiceMock::$requestResponse = "";
+        $result = CheckoutServiceMock::redeemVoucher(self::VOUCHER_ID_1, self::CUSTOMER_ID, false);
+
+        $this->assertFalse(is_null($result));
+        $this->assertTrue(is_bool($result));
+        $this->assertTrue($result);
+
+        $this->assertError(E_USER_NOTICE, "GET URL CALL");
+    }
+
+    public function testRedeemVoucherFalse() {
+        $data = json_decode('{"error":"404","description":"Voucher ID not found"}');
+        CheckoutServiceMock::$requestResponse = $data;
+
+        try {
+            $result = CheckoutServiceMock::redeemVoucher(self::VOUCHER_ID_1, self::CUSTOMER_ID, false);
             $this->assertTrue(false, "Method should have thrown an error");
         } catch (\Exception $exc) {
             $this->assertTrue(true);
